813cfcd78dce38e2c886e5d381b7f6051430f4b5
ts-liqun.tang <ts-liqun.tang@rakuten.com>
2021-07-02 11:23:40 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let somevalue = 123;
+    println!("Hello, world! {}", somevalue);
+}
+// the exclamation mark marks this as a macro, and not a function
+// i dunno what's a macro yet
